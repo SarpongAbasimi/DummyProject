f5fa9c91eaba227d615083bf9aa4448c654059a0
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-05-27 19:23:06 +0100

    [SA] Clean up for the next phase of application

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -5,27 +5,8 @@ import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
 import scala.concurrent.ExecutionContext.global
 
-/**
- * This should prob be moved to a config file
- */
-object HttpBinding {
-  val port = 8080
-  val host = "localhost"
-}
-
 object Server {
-  import HttpBinding._
-  import ApplicationRoutes._
 
-  def stream[F[_]: Timer: ConcurrentEffect]: Stream[F, ExitCode] = {
-    for {
-      client <- BlazeClientBuilder[F](global).stream
-      githubClient = GitHubClient.imp[F](client)
-      services     = (contributorsWrapper[F](githubClient)).orNotFound
-      exitCode <- BlazeServerBuilder[F](global)
-        .bindHttp(port, host)
-        .withHttpApp(services)
-        .serve
-    } yield exitCode
-  }
+  def stream[F[_]: Timer: ConcurrentEffect]: Stream[F, ExitCode] =
+    ???
 }
