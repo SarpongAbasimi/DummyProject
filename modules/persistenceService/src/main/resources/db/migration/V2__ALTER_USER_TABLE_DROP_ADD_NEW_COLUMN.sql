4ccee02b98133d54e18de484bed52d862cf95dc4
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-08-10 09:14:42 +0100

    [SA] Updated db table

---
+++ modules/persistenceService/src/main/resources/db/migration/V2__ALTER_USER_TABLE_DROP_ADD_NEW_COLUMN.sql
@@ -0,0 +1,19 @@
+ALTER TABLE userdb
+DROP COLUMN username,
+DROP COLUMN name,
+ADD COLUMN slack_user_id VARCHAR UNIQUE NOT NULL,
+ADD COLUMN slack_channel_id VARCHAR UNIQUE NOT NULL;
+
+CREATE TABLE repositories(
+    repository_id uuid PRIMARY KEY NOT NULL,
+    owner VARCHAR NOT NULL,
+    repository VARCHAR NOT NULL
+);
+
+CREATE TABLE subscriptions(
+    id uuid NOT NULL,
+    repository_id uuid NOT NULL,
+    PRIMARY KEY(id, repository_id),
+    FOREIGN KEY (id) REFERENCES userdb(id) ON UPDATE CASCADE,
+    FOREIGN KEY (repository_id) REFERENCES repositories(repository_id) ON UPDATE CASCADE
+);
