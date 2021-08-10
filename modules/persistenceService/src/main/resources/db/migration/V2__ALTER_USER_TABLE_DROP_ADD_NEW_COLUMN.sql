9e2ddd68d15bc146fb4674456acbd0e99eb06778
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-08-10 12:31:33 +0100

    [SA] changed column types to uuid

--- modules/persistenceService/src/main/resources/db/migration/V2__ALTER_USER_TABLE_DROP_ADD_NEW_COLUMN.sql
+++ modules/persistenceService/src/main/resources/db/migration/V2__ALTER_USER_TABLE_DROP_ADD_NEW_COLUMN.sql
@@ -1,8 +1,8 @@
 ALTER TABLE userdb
 DROP COLUMN username,
 DROP COLUMN name,
-ADD COLUMN slack_user_id VARCHAR UNIQUE NOT NULL,
-ADD COLUMN slack_channel_id VARCHAR UNIQUE NOT NULL;
+ADD COLUMN slack_user_id uuid UNIQUE NOT NULL,
+ADD COLUMN slack_channel_id uuid UNIQUE NOT NULL;
 
 CREATE TABLE repositories(
     repository_id uuid PRIMARY KEY NOT NULL,
