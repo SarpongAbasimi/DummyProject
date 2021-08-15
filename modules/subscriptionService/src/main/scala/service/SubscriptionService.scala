795793b9a38bf7ff113d86ec311af3e23357c365
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-08-15 14:17:05 +0100

    Added check to verify if user exixts firt before getting data

--- modules/subscriptionService/src/main/scala/service/SubscriptionService.scala
+++ modules/subscriptionService/src/main/scala/service/SubscriptionService.scala
@@ -1,6 +1,7 @@
 package service
 
 import cats.effect.Sync
+import userDbAlgebra.UserAlgebra
 import subscriptionAlgebra.SubscriptionServiceAlgebra
 import utils.Types.{GetSubscriptionData, Id, PostSubscriptions}
 import doobie.ConnectionIO
@@ -9,18 +10,45 @@ import doobie.util.transactor.Transactor
 
 object SubscriptionService {
   def implementation[F[_]: Sync](
+      userAlgebra: UserAlgebra[ConnectionIO],
       subscriptionAlgebra: SubscriptionServiceAlgebra[ConnectionIO],
       transactor: Transactor[F]
   ): SubscriptionServiceAlgebra[F] = new SubscriptionServiceAlgebra[F] {
-    def get(id: Id): F[Option[GetSubscriptionData]] =
-      subscriptionAlgebra
-        .get(id)
-        .transact(transactor)
+    def get(id: Id): F[Option[GetSubscriptionData]] = {
+      for {
+        user <- userAlgebra.findUser(id)
+        listOfSubscriptions <- user match {
+          case None =>
+            Sync[ConnectionIO]
+              .raiseError(
+                new Exception("user does not exit")
+              )
+          case Some(user) => subscriptionAlgebra.get(user.id)
+        }
+      } yield listOfSubscriptions
+    }.transact(transactor)
 
-    def post(id: Id, subscriptions: PostSubscriptions): F[Unit] =
-      subscriptionAlgebra.post(id, subscriptions).transact(transactor)
+    def post(id: Id, subscriptions: PostSubscriptions): F[Unit] = (for {
+      checkIfTheUserExits <- userAlgebra.findUser(id)
+      _ <- checkIfTheUserExits match {
+        case None =>
+          Sync[ConnectionIO]
+            .raiseError(
+              new Exception(
+                s"Invalid: User with ${id.id} does not exit"
+              )
+            )
+        case Some(user) => subscriptionAlgebra.post(user.id, subscriptions)
+      }
+    } yield ()).transact(transactor)
 
-    def delete(id: Id, subscriptions: PostSubscriptions): F[Unit] =
-      subscriptionAlgebra.delete(id, subscriptions).transact(transactor)
+    def delete(id: Id, subscriptions: PostSubscriptions): F[Unit] = (for {
+      user <- userAlgebra.findUser(id)
+      _ <- user match {
+        case None =>
+          Sync[ConnectionIO].raiseError(new Exception(s"Invalid: User with ${id.id} does not exit"))
+        case Some(user) => subscriptionAlgebra.delete(id, subscriptions)
+      }
+    } yield ()).transact(transactor)
   }
 }
